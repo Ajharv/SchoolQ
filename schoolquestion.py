0860bada100ab43882c5378b252cdd1d5e1b7768
Alex Harvey <ajharvey11@gmail.com>
2021-04-09 13:25:35 -0400

    moved

---
+++ schoolquestion.py
@@ -0,0 +1,29 @@
+import RANDOM
+
+# sayings must be a list of sayings
+sayings =
+    'Today will be a wonderful day!',
+    'You are going to realize your dreams!',
+    'Soon your ideas will be appreciated by others!',
+    'Doors are opening because of your hard work!',
+    'Your dreams are about to come true.'
+
+count_sayings = 10
+
+get_saying = true
+
+# enter into a loop while the user wants to continue to play
+while get_saying = true:
+    saying_index = randint(0, count_sayings) # get a random index value
+    print('\n')
+    print('----- Your Saying for Today -----')
+    print(sayings) # print the random saying
+
+    print('\n')    
+    user_response = 'Would you like another inspirational saying? (y/n) '
+
+    if response = 'y':
+        saying = False;
+
+#print final message as the game play ends.
+    print('Thank you for playing, please come again.')
